ed3f0dd8864679fb8bb1d04082cd273b454f4283
Jussivan <jussivanbm1029@gmail.com>
2025-07-10 21:33:15 -0300

    add create-question route

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,7 +2,10 @@ import { fastify } from 'fastify';
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
 import { fastifyCors} from '@fastify/cors';
 import { env } from './env.ts';
+//Routes
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { createQuestionRoute } from './http/routes/create-question.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -18,6 +21,9 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(createQuestionRoute);
+
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('Server is running on http://localhost:3333');
